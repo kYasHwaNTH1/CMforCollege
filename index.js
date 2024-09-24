90b30b39f0e5252859a85ae72f844f04064f3d4f
kYasHwaNTH1 <kesagoniyashwanth@gmail.com>
2024-09-24 22:42:48 +0530

    done with basic setting not all

--- index.js
+++ index.js
@@ -1,6 +1,23 @@
 const express=require('express')
+const cors=require('cors')
+const mongoose=require('mongoose')
 const app=express()
+require('dotenv').config()
 
-app.listen(3002,()=>{
-    console.log('server is running at 3002 port')
-})
+
+app.use(express.json())
+app.use(cors())
+
+
+async function main() {
+    try {
+        await mongoose.connect(process.env.MONGODB_URL, { useNewUrlParser: true, useUnifiedTopology: true });
+        console.log('Connected to Database Successfully');
+        app.listen(3002, () => {
+            console.log("Listening on 3002");
+        });
+    } catch (err) {
+        console.error("Database connection error:", err.message);
+    }
+}
+main()
